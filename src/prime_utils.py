cea0ec52abffda886076e074255996952b5def49
Eric Page <genamthi@gmail.com>
2025-06-02 00:45:30 -0700

    Simplified the test structure. Rebuilding base class (work in progress) so that we can use the general case with filtering. Added optimizations for prime power summands, prime arguments, and general factor sums by using list comprehensions and basic facts about multiplication. Currently have an experimental algorithm that will be moved to only the experimental branch as I test out how to use young lattices and symmetries to do this more efficiently.

--- src/prime_utils.py
+++ src/prime_utils.py
@@ -10,7 +10,10 @@ def is_prime(n):
     Returns:
         bool: True if n is prime, False otherwise.
     """
+<<<<<<< HEAD
     #Consider wrapper with memoization for algorithms that make multiple calls
+=======
+>>>>>>> 610264d (Simplified the test structure.)
     if n < 2:
         return False
     if n == 2:
@@ -22,4 +25,8 @@ def is_prime(n):
     for d in range(3, limit, 2):
         if n % d == 0:
             return False
+<<<<<<< HEAD
     return True 
+=======
+    return True 
+>>>>>>> 610264d (Simplified the test structure.)
